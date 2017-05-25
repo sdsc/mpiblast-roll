371d8c01524a893509142a847d248bd9cadcd8d0
Jim Hayes <jhayes@sdsc.edu>
2017-05-25 12:02:01 -0700

    Remove cuda from rpm description.
    Resolves APS-470.

--- src/mpiblast/version.mk
+++ src/mpiblast/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-mpiblast
 VERSION        = 1.6.0
-RELEASE        = 3
+RELEASE        = 4
 PKGROOT        = /opt/mpiblast
 
 SRC_SUBDIR     = mpiblast
